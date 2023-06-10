02903ea0f09dad752b4f61ba04998d018977f8a5
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2023-06-10 20:40:06 +0530

    initial commit

---
+++ DevShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevShare
+//
+//  Created by Saurabh Jamadagni on 10/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevShare/DevShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevShareApp.swift
+//  DevShare
+//
+//  Created by Saurabh Jamadagni on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DevShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
